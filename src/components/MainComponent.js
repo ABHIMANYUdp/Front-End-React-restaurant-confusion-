54d252e18293cb3d96f11385f18b646d9b0943bb
Nishit Mistry <nishitmistry07223@gmail.com>
2020-09-18 17:01:32 +0530

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import Home from "./HomeComponent";
+import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
@@ -53,6 +54,11 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
+          <Route
+            exact
+            path='/aboutus'
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Route
             exact
             path='/menu'
